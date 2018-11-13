09a4064176a418b3519d1c60ac3aa1a5edbfa6a9
Takeshi69 <qdudon@gmail.com>
2018-11-13 09:25:52 +0100

    Starter Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {NgModule, OnInit} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {environment} from '../environments/environment';
@@ -8,14 +8,28 @@ import {APP_CONFIG} from './app.config';
 import {TokenInterceptorService} from './services/interceptors/token-interceptor.service';
 import {ErrorInterceptorService} from './services/interceptors/error-interceptor.service';
 import {CommonHeadersInterceptorService} from './services/interceptors/common-headers-interceptor.service';
+// import {RouterModule} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
+import {MaterialModule} from './material.module';
+import {LoginComponent} from './Pages/Connexion/login/login.component';
+import {SignInComponent} from './Pages/Connexion/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
     {provide: APP_CONFIG, useValue: environment},
