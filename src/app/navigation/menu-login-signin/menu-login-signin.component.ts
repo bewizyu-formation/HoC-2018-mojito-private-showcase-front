e10abfa7445bde7c6429ad78cb37846e8f13c165
Yassin Assim <38948395+yassinHOC@users.noreply.github.com>
2018-11-14 11:30:43 +0100

    Ajout des routes et navigations (jusqu'a Home) (#4)
    Tu es le plus fort!

---
+++ src/app/navigation/menu-login-signin/menu-login-signin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-login-signin',
+  templateUrl: './menu-login-signin.component.html',
+  styleUrls: ['./menu-login-signin.component.css']
+})
+export class MenuLoginSigninComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
