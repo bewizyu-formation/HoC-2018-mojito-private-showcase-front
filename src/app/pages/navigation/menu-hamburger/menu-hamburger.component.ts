399ae8518cbddc818f9e9e30242cb632159601e6
Faresse TAHLAITI <faresse.tahlaiti@aol.com>
2018-11-20 17:00:22 +0100

    3 Routes de fait et pages events

--- src/app/pages/navigation/menu-hamburger/menu-hamburger.component.ts
+++ src/app/pages/navigation/menu-hamburger/menu-hamburger.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-menu-hamburger',
@@ -7,8 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuHamburgerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  navigateToEvents() {
+    this.router.navigate(['events']);
+  }
+
+  navigateToHome() {
+    this.router.navigate(['home']);
+  }
+
+  navigateToContacts() {
+    this.router.navigate(['contact']);
+  }
   ngOnInit() {
   }
 
