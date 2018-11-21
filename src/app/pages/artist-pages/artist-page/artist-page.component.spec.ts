a850c5eff7f501eca7e00d5b8c6d9601b637ce91
Yassin Assim <yassinassim0@gmail.com>
2018-11-21 15:29:03 +0100

    Correction des conflits2

--- src/app/pages/artist-pages/artist-page/artist-page.component.spec.ts
+++ src/app/pages/artist-pages/artist-page/artist-page.component.spec.ts
@@ -1,5 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import {ArtistPageComponent} from './artist-page.component';
+import {MaterialModule} from '../../../material.module';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
+import {ArtistPageFormComponent} from '../artist-page-form/artist-page-form.component';
+import {ArtistPagePresentationComponent} from '../artist-page-presentation/artist-page-presentation.component';
 
 
 describe('ArtistPageComponent', () => {
@@ -8,7 +14,18 @@ describe('ArtistPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ArtistPageComponent ],
+      declarations: [
+        ArtistPageComponent,
+        ArtistPageFormComponent,
+        ArtistPagePresentationComponent
+      ],
+      imports: [
+        MaterialModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule,
+        HttpClientModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
