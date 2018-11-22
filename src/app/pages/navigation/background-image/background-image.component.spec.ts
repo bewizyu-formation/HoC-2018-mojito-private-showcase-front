e73255e00b6c2e96154cd339fa34273cba8fb102
Takeshi69 <qdudon@gmail.com>
2018-11-22 17:44:13 +0100

    Routes for navbar double ok

--- src/app/pages/navigation/background-image/background-image.component.spec.ts
+++ src/app/pages/navigation/background-image/background-image.component.spec.ts
@@ -23,8 +23,4 @@ describe('BackgroundImageComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
