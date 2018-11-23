d30fd794278022ccaacad494707a7e3b93273420
Takeshi69 <qdudon@gmail.com>
2018-11-23 11:41:20 +0100

    Create Page Fiche Artist Ok !!!

--- src/app/pages/artist-pages/artist-page-presentation/artist-page-presentation.component.spec.ts
+++ src/app/pages/artist-pages/artist-page-presentation/artist-page-presentation.component.spec.ts
@@ -28,8 +28,4 @@ describe('ArtistPagePresentationComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
